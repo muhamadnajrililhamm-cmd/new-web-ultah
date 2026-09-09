4e0fda2ca268d381ef99ed9f80ef3897fff3ef9c
muhamadnajrililhamm-cmd <muhamadnajrililhamm@gmail.com>
2026-09-09 22:27:33 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './style.css';
 
 const CONFIG={
  name:'Savira', senderName:'Aku', pin:'021128', birthdayDate:'02 November',
- whatsapp:'628123456789', music:'/music/our-song.mp3',
- photos:{cover:'https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2?auto=format&fit=crop&w=1200&q=85',letter1:'https://images.unsplash.com/photo-1520857014576-2c4f4c972b57?auto=format&fit=crop&w=1200&q=85',letter2:'https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=1200&q=85',letter3:'https://images.unsplash.com/photo-1518199266791-5375a83190b7?auto=format&fit=crop&w=1200&q=85',letter4:'https://images.unsplash.com/photo-1513279922550-250c2129b13a?auto=format&fit=crop&w=1200&q=85',special:'https://images.unsplash.com/photo-1494774157365-9e04c6720e47?auto=format&fit=crop&w=1200&q=85'},
+ whatsapp:'6283853276614', music:'/music/our-song.mp3',
+ photos:{cover:'https://raw.githubusercontent.com/armangzoffc/uploader-nusantara/main/Uploads/1788967547460-e86f9332.jpg',letter1:'https://raw.githubusercontent.com/armangzoffc/uploader-nusantara/main/Uploads/1788967547460-e86f9332.jpg',letter2:'https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=1200&q=85',letter3:'https://images.unsplash.com/photo-1518199266791-5375a83190b7?auto=format&fit=crop&w=1200&q=85',letter4:'https://images.unsplash.com/photo-1513279922550-250c2129b13a?auto=format&fit=crop&w=1200&q=85',special:'https://images.unsplash.com/photo-1494774157365-9e04c6720e47?auto=format&fit=crop&w=1200&q=85'},
  gifs:{birthday:'https://media.giphy.com/media/g9582DNuQppxC/giphy.gif',love:'https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif',flower:'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif'}
 };
 const letters=[
